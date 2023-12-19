e5ad3bbf68ad4f20cf67c4d61a7034b80e688757
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-12-18 23:16:15 -0600

    Creando un metodo para animales

--- POO.js
+++ POO.js
@@ -4,6 +4,11 @@ class animal {
         this.edad = edad;
         this.color = color
         this.informacion = `Soy ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`
+        //*METODO
+       
+    }
+    verInformacion(){
+        document.write(this.informacion+"<br>")
     }
 }
 
@@ -15,4 +20,14 @@ let pajaro = new animal("Loro",12,"Verde")
  
 //*ES UN OBJETO ASI QUE PARA ACCEDER A LAS PROPIEDADES ES CON UN '.'
 
-document.write(pajaro.informacion)
+// document.write(pajaro.informacion+"<br>")
+
+// document.write(perro.informacion+"<br>")
+
+// document.write(gato.informacion+"<br>")
+
+/*SI LA PONEMOS FUERA ES FUNCION, SI LA TENEMOS DENTRO DEL OBJETO SE CONOCE COMO METODO*/
+
+perro.verInformacion();//USO DEL METODO
+gato.verInformacion();
+pajaro.verInformacion();
