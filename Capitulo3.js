208cc50c2b28480aec8917a89fa4020d1dd828d9
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-12-19 00:06:33 -0600

    Iniciando el capitulo 3

---
+++ Capitulo3.js
@@ -0,0 +1,93 @@
+/**HISTORIA DE COFLA 3 
+ * CREAR UN SISTEMA PARA MOSTRARLE A COFLA LAS PARTICULARIDADES E LOS 3 CELULARES.
+ * CADA CELULAR DEBE TENER COLOR, PESO, RESOLUCION DE PANTALLA, RESOLUCION DE CAMARA Y MEMORIA RAM, CADA CELULAR DEBE PODER PRENDER, REINICIAR, APAGAR , TOMAR FOTOS Y GRABAR
+*/
+
+class celphone{
+    constructor(color, peso, tamaño, rdc, ram){
+        this.color = color;
+        this.peso = peso;
+        this.tamaño = tamaño;
+        this.resolucionDeCamara = rdc;
+        this.memoriaRam = ram;
+        this.encendido = false;
+    }
+    encenderPantalla(){
+        if(this.encendido == false){
+            alert("celular prendido")
+            this.encendido = true;
+        }else{
+            alert("el celular ya esta prendido")
+        }
+    }
+
+    apagar(){
+        if(this.encendido == true){
+            alert("celular apagado")
+            this.encendido = false;
+        }else{
+            alert("el celular ya esta apagado")
+        }
+    }
+
+    reiniciar(){
+        if(this.encendido == true){
+            alert("reiniciando celular")
+        }else{
+            alert("el celular esta apagado")
+        }
+    }
+    
+    tomarFotos(){
+        alert(`foto tomada en una resolucion de: ${this.resolucionDeCamara}`)
+    }
+
+    grabarVideo(){
+        alert(`grabando video en una resolucion de: ${this.resolucionDeCamara}`)
+    }
+
+    mostrarInfo(){
+        return `
+        Color: <b>${this.color}</b><br>
+        Peso: <b>${this.peso}</b><br>
+        Resolucion de Pantalla: <b>${this.tamaño}</b><br>
+        Resolucion de Camara: <b>${this.resolucionDeCamara}</b><br>
+        Memoria Ram: <b>${this.memoriaRam}</b><br>
+        `
+    }
+
+}
+
+const celular1 = new celphone("Rojo", "150g", "5'", "4k", "3GB");
+const celular2 = new celphone("Azul", "160g", "6'", "1080p", "4GB");
+const celular3 = new celphone("Negro", "140g", "5.5'", "720p", "2GB");
+
+
+// celular1.encenderPantalla();
+// celular1.tomarFotos();
+// celular1.grabarVideo();
+// celular1.reiniciar();
+// celular1.encenderPantalla()
+
+
+document.write(`
+    ${celular1.mostrarInfo()}<br> 
+    ${celular2.mostrarInfo()}<br> 
+    ${celular3.mostrarInfo()}<br> `
+)
+
+class celphonePlus extends celphone{
+    constructor(color, peso, tamaño, ram, rdce){
+        super(color, peso, tamaño, tamaño, ram);
+        this.rdce = rdce
+    }
+
+    grabarVideoLento(){
+        alert('Estas grabando en camara lenta');
+    }
+
+    reconocimiento(){
+        alert('Vamos a iniciar un reconocimiento facial')
+    }
+}
+
